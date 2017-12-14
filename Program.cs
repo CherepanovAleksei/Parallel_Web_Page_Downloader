d69e8049423e5199715fbe271d20e7f69c4f9884
CherepanovAleksei <mrneumann888@gmail.com>
2017-12-15 02:01:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WebPageDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch time = new Stopwatch();
+            WebPageDownloader WPD = new WebPageDownloader();
+
+            Console.WriteLine("Enter URI:");
+            input:
+            try
+            {
+                Uri input = new Uri(Console.ReadLine());
+                time.Start();
+                WPD.Download(input);
+                time.Stop();
+                Console.WriteLine("Time: {0}", time.Elapsed);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Wrong input. Try again:");
+                goto input;
+            }
+            catch(UriFormatException)
+            {
+                Console.WriteLine("Wrong input. Try again:");
+                goto input;
+            }
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WebPageDownloader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace WebPageDownloader
+{
+    class WebPageDownloader
+    {
+        public void Download(Uri parent)
+        {
+            var WCParent = new WebClient();
+            string pattern = @"href=""(?<url>http(s)?[\w\.:?&-_=#/]*)""+?";
+            Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+            List<string> links = new List<string>();
+
+            string parentString = WCParent.DownloadString(parent);
+            Console.WriteLine("This is {0} => {1}", parent, parentString.Length.ToString());
+
+            MatchCollection matches = rgx.Matches(parentString);
+
+            foreach (Match match in matches)
+            {
+                links.Add(match.ToString().Split(new Char[] { '"' })[1]);
+            }
+
+            if (links != null)
+            {
+                try
+                {
+                    Task.WaitAll(links.Select(DownloadChildren).ToArray());
+                }
+                catch { }
+            }
+        }
+
+        private async Task DownloadChildren(string address)
+        {
+            using (var client = new WebClient())
+            {
+                var uri = new Uri(address);
+                client.DownloadDataCompleted += Result;
+                client.Headers["address"] = address;
+                await client.DownloadDataTaskAsync(uri);
+            }
+        }
+
+        private void Result(object obj, DownloadDataCompletedEventArgs e)
+        {
+            if (obj is WebClient client && !e.Cancelled && e.Error == null)
+            {
+                Console.WriteLine("{0} => {1}", client.Headers["address"], client.Encoding.GetString(e.Result).Length);
+            }
+        }
+    }
+}
